0d4d923b8e4225b9576f05c384430a5024b31c26
michaelKurowski <Byamarro@gmail.com>
2018-07-28 00:35:55 +0200

    Added option to run server silently

--- index.js
+++ index.js
@@ -6,22 +6,25 @@ const commandArgumentsList = process.argv.slice(2)
 
 const commandArgumentsMap = {
     port: commandArgumentsList[0],
-    closeAfterFirstPacket: commandArgumentsList[1]
+    closeAfterFirstPacket: commandArgumentsList[1],
+    outputOnlyPackets: commandArgumentsList[2]
 }
 
 const port = commandArgumentsMap.port || DEFAULT_PORT
 
 function onError(error) {
-    console.log(`Error occured ${error}`)
+    if (!outputOnlyPackets) console.log(`Error occured ${error}`)
 }
 
 function onMessage(message, remoteAddressInformation) {
-    console.log(`Received message: ${message} from ${remoteAddressInformation.address}:${remoteAddressInformation.port}`)
+    if (!outputOnlyPackets)
+        console.log(`Received message: ${message} from ${remoteAddressInformation.address}:${remoteAddressInformation.port}`)
+    if (outputOnlyPackets) console.log(message)
     if (commandArgumentsMap.closeAfterFirstPacket === 'true') process.exit()
 }
 
 function onServerListening() {
-    console.log(`DHCP Server is listening on port ${port}`)
+    if (!outputOnlyPackets) console.log(`DHCP Server is listening on port ${port}`)
 }
 
 const dhcpServer = dhcpServerCreator(port, onError, onMessage, onServerListening)
