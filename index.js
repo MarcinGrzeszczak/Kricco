bc088aa0e0b3258c20cdc929e4240e923bee88c9
michaelKurowski <Byamarro@gmail.com>
2018-07-28 01:58:34 +0200

    Deleted Marcin's parser files

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
 const dhcpServerCreator = require('./dhcpServerCreator')
-const packageParser = require('./packageParser')
 const handleArguments = require('./cli/handleArguments')
 const args = handleArguments()
 
