114f42bf1b61d7e24688a9cb89b0f53e04e2fdd1
michaelKurowski <Byamarro@gmail.com>
2018-07-28 22:33:34 +0200

    DHCP properties code formatting

--- dhcpParser/dhcpProperties.js
+++ dhcpParser/dhcpProperties.js
@@ -1,6 +1,8 @@
 const DhcpOptionProperty = require('./DhcpOptionPropertyClass')
 const protocolParsingUtils= require('../protocolParsingUtils')
 module.exports = {
-    LENGTH: new DhcpOptionProperty('length', protocolParsingUtils.parseTo8UInt),
-    PARAMETER_REQUEST_LIST: new DhcpOptionProperty('Parameter Request List', protocolParsingUtils.parseToListOf8UInts)
+    LENGTH:
+        new DhcpOptionProperty('length', protocolParsingUtils.parseTo8UInt),
+    PARAMETER_REQUEST_LIST:
+        new DhcpOptionProperty('Parameter Request List', protocolParsingUtils.parseToListOf8UInts)
 }
