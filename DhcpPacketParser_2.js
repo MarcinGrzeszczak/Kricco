4ad7615196c62dd8b5a4e1b5109f0dba8a235c37
michaelKurowski <Byamarro@gmail.com>
2018-07-28 19:43:42 +0200

    Treating chaddr 2 first lines as single mac

--- DhcpPacketParser_2.js
+++ DhcpPacketParser_2.js
@@ -3,12 +3,7 @@ function parse(message) {
 	const result = {
 		siaddr: protocolParsingUtils.parseIp(message, 20),
 		giaddr: protocolParsingUtils.parseIp(message, 24),
-		chaddr: [
-			protocolParsingUtils.parseMac(message, 28),
-			protocolParsingUtils.parseMac(message, 32),
-			protocolParsingUtils.parseMac(message, 36),
-			protocolParsingUtils.parseMac(message, 40)
-		]
+		chaddr: protocolParsingUtils.parseMac(message, 28)
 	}
 	console.log(result)
 	return message
