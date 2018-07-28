bf1bf3ea6e96f066e111c4801488eadcc08ea23c
Marcin Grzeszczak <marcin.grzeszczak@gmail.com>
2018-07-28 18:14:25 +0200

    first parsing

--- DhcpPacketParser_1.js
+++ DhcpPacketParser_1.js
@@ -1,6 +1,15 @@
 
 function parse(message) {
-	return message
+	frameDictionary = {
+		'OP' : message.readInt8(0),
+		'HTYPE': message.readInt8(1),
+		'HLEN': message.readInt8(2),
+		'HOPS': message.readInt8(3),
+		'XID' : message.readInt32BE(1),
+		'SECS': message.readInt16BE(5)
+
+	}
+	return frameDictionary
 }
 
 module.exports = parse
