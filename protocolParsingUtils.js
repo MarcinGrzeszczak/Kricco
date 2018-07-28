1b3fb3700d4b6e3652650df942d9589303f54c1a
michaelKurowski <Byamarro@gmail.com>
2018-07-28 19:26:45 +0200

    Changed int to uint in parseIp

--- protocolParsingUtils.js
+++ protocolParsingUtils.js
@@ -3,5 +3,9 @@ module.exports = {
 }
 
 function parseIp(buffer, offset) {
+    return `${buffer.readUInt8(offset)}.${buffer.readUInt8(offset+1)}.${buffer.readUInt8(offset+2)}.${buffer.readUInt8(offset+3)}`
+}
+
+function parseMac(buffer, offset) {
     return `${buffer.readInt8(offset)}.${buffer.readInt8(offset+1)}.${buffer.readInt8(offset+2)}.${buffer.readInt8(offset+3)}`
 }
