51e4633bbd39480332c0eb987014aee5b1ee95c2
michaelKurowski <Byamarro@gmail.com>
2018-07-28 20:26:53 +0200

    Moved parsign magic cookie offset to 240

--- DhcpPacketParser_2.js
+++ DhcpPacketParser_2.js
@@ -4,7 +4,7 @@ function parse(message) {
 		siaddr: protocolParsingUtils.parseIp(message, 20),
 		giaddr: protocolParsingUtils.parseIp(message, 24),
 		chaddr: protocolParsingUtils.parseMac(message, 28),
-		magicCookie: message.toString('hex', 200, 200 + 4)
+		magicCookie: message.toString('hex', 240, 240 + 4)
 	}
 	console.log(result)
 	return message
