0e238203f61d6e0747db5f2301361663524f1ca6
michaelKurowski <Byamarro@gmail.com>
2018-07-28 18:53:58 +0200

    Changed slicing step

--- dhcpPacketSplitter.js
+++ dhcpPacketSplitter.js
@@ -1,10 +1,25 @@
+const LINES = [
+    'OP, HLEN, HTYPE, HOPS',
+    'XID',
+    'SECS, FLAGS',
+    'CIADDR',
+    'YIADDR',
+    'SIADDR', 
+    'GIADDR',
+    'CHADDR:1',
+    'CHADDR:2',
+    'CHADDR:3',
+    'CHADDR:4',
+    'MAGGIC COOKIE'
+]
+
 
 function dhcpPacketLinesParser(buffer) {
-    const step = 32
+    const step = 4
     let result = []
     for (let i = 0 ; i + step <= buffer.length ; i += step) {
         const dhcpLine = buffer.slice(i, i + step)
-        console.log('Line: ', i, dhcpLine)
+        console.log('Line: ', LINES[i], dhcpLine)
         result.push(dhcpLine)
     }
     return result
