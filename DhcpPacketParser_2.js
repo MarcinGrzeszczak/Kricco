fe51de2d51968ad2f6c453732f4127d1ea30bc9f
michaelKurowski <Byamarro@gmail.com>
2018-07-28 20:10:35 +0200

    Magic cookie parsing

--- DhcpPacketParser_2.js
+++ DhcpPacketParser_2.js
@@ -3,7 +3,8 @@ function parse(message) {
 	const result = {
 		siaddr: protocolParsingUtils.parseIp(message, 20),
 		giaddr: protocolParsingUtils.parseIp(message, 24),
-		chaddr: protocolParsingUtils.parseMac(message, 28)
+		chaddr: protocolParsingUtils.parseMac(message, 28),
+		magicCookie: message.toString('hex', 88, 88 + 4)
 	}
 	console.log(result)
 	return message
