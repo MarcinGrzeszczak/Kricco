a2ee0ff4ca34da252fb4a705a4387105652104f8
michaelKurowski <Byamarro@gmail.com>
2018-07-28 18:44:20 +0200

    Logging spearated lines

--- dhcpPacketSplitter.js
+++ dhcpPacketSplitter.js
@@ -3,8 +3,8 @@ function dhcpPacketLinesParser(buffer) {
     const step = 32
     let result = []
     for (let i = 0 ; i + step <= buffer.length ; i += step) {
-        console.log('Reading from offset:', i)
         const dhcpLine = buffer.slice(i, i + step)
+        console.log('Line: ', i, dhcpLine)
         result.push(dhcpLine)
     }
     return result
