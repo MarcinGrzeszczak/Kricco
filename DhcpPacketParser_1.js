401e60580a6e3c7ef82f1f5ba81a405448cc1f47
Marcin Grzeszczak <marcin.grzeszczak@gmail.com>
2018-07-28 19:00:03 +0200

    Parsing frames OP ... YIADDR

--- DhcpPacketParser_1.js
+++ DhcpPacketParser_1.js
@@ -1,12 +1,19 @@
 
+function parserIP(uIntNumber) {
+	return uIntNumber
+}
+
 function parse(message) {
 	frameDictionary = {
 		'OP' : message.readInt8(0),
 		'HTYPE': message.readInt8(1),
-		'HLEN': message.readInt8(2),
-		'HOPS': message.readInt8(3),
-		'XID' : message.readInt32BE(1),
-		'SECS': message.readInt16BE(5)
+		'HLEN': message.readUInt8(2),
+		'HOPS': message.readUInt8(3),
+		'XID' : message.readUInt32BE(4),
+		'SECS': message.readUInt16BE(8),
+		'FLAGS': message.readUInt16BE(10),
+		'CIADDR': parserIP(message.readUInt32BE(12)),
+		'YIADDR': parserIP(message.readUInt32BE(16))
 
 	}
 	return frameDictionary
