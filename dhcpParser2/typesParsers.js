924c6c04ad43fbd3c16316f39baf091005d261bb
michaelKurowski <Byamarro@gmail.com>
2018-09-02 17:18:26 +0200

    List of types parsers

--- dhcpParser2/typesParsers.js
+++ dhcpParser2/typesParsers.js
@@ -6,6 +6,23 @@ const dictionary = {
         serialize: number => Buffer.alloc(8).writeUInt8(number),
         deserialize: buffer => buffer.readUInt8(),
         size: 8
+    },
+    string: {
+        serialize: string => Buffer.alloc(1).write(string),
+        deserialize: buffer => buffer.toString('utf8'),
+        size: 1
+    },
+    ipv4: {
+        serialize: ip => {
+            const parsedOctets = string.split('.').map(parseInt)
+            return Buffer.alloc(32).writeUInt8(parsedOctets)
+        },
+        deserialize: buffer => {
+            let arrayToFill = [].fill(0, 0, 4)
+            const listOfOctets = arrayToFill.map((value, index) => Buffer.readUInt8(index))
+            return listOfOctets.join('.')
+        },
+        size: 32
     }
 }
 
