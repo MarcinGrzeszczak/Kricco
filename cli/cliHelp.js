f7d9eb6b771b7c5105a17c615d86ce298ddb4b02
michaelKurowski <Byamarro@gmail.com>
2018-09-07 22:19:32 +0200

    Fixed CLI help formatting

--- cli/cliHelp.js
+++ cli/cliHelp.js
@@ -2,7 +2,7 @@ module.exports =
 `
 DHCP Server
     Options:
-        --port,     -p  <number>            Port to listen on. 67 by default
-        --fileToParse,    -f  <path>        DHCP file with dumped data to parse
+        --port,          -p  <number>      Port to listen on. 67 by default
+        --fileToParse,   -f  <path>        DHCP file with dumped data to parse
 
 `
