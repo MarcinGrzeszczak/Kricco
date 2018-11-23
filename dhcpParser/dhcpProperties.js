b596e57c5a325bd505bef208db1eb2ed83f9a7dd
michaelKurowski <Byamarro@gmail.com>
2018-11-23 19:41:38 +0100

    Broadcast Address (28)

--- dhcpParser/dhcpProperties.js
+++ dhcpParser/dhcpProperties.js
@@ -5,6 +5,11 @@ const typesParsers = require('./typesParsers')
 const FORMATTERS = require('./formatters')
 
 const dhcpProperties = {
+	BROADCAST_ADDRESS: {
+		name: 'Broadcast Address',
+		isList: false,
+		typeParser: typesParsers.ipv4
+	},
 	SUBNET_MASK: {
 		name: 'Subnet Mask',
 		isList: false,
